0a3aa96a3f4aabc13fcda7df4360b21786a38a85
crccode <54750603+crccode@users.noreply.github.com>
2023-07-04 20:34:51 -0400

    ESTRUCTURA BASICA DE UN CONTEXT

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import AppSeguro from './components/AppSeguro'
+import { CotizadorProvider } from './context/CotizadorProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <CotizadorProvider>
       <AppSeguro/>
-    </div>
+    </CotizadorProvider>
   )
 }
 
---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,19 @@
+import { useState, createContext } from 'react'
+const CotizadorContext = createContext()
+
+const CotizadorProvider = ({children}) => {
+    return (
+        <CotizadorContext.Provider 
+            value={{
+
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+
+    )
+}
+export {
+    CotizadorProvider
+}
+export default CotizadorContext
