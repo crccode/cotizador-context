81fbb5c6e5ecce3310c2ab487b9787f8e11a0cfb
crccode <54750603+crccode@users.noreply.github.com>
2023-07-04 20:12:49 -0400

    CREANDO EL APPSEGURO Y FROMULARIO

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import AppSeguro from './components/AppSeguro'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>hola</h1>
+      <AppSeguro/>
     </div>
   )
 }
